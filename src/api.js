834b9271d74f99e0236bec6bcd0b48f42cf1788f
Rakshitha-abc <rakshitharashu60@gmail.com>
2026-03-02 08:24:37 +0530

    fix: protocol to https to avoid mixed content error on deployment

--- src/api.js
+++ src/api.js
@@ -1,43 +1,43 @@
 import axios from 'axios';
 
 const API_KEY = '9d5763138301c0ffbb2b1adfa4518a70';
-const BASE_URL = 'http://api.weatherstack.com';
+const BASE_URL = 'https://api.weatherstack.com';
 
 const apiClient = axios.create({
-  baseURL: BASE_URL,
-  params: {
-    access_key: API_KEY,
-  },
+    baseURL: BASE_URL,
+    params: {
+        access_key: API_KEY,
+    },
 });
 
 export const weatherApi = {
-  getCurrent: async (query) => {
-    const { data } = await apiClient.get('/current', { params: { query } });
-    if (data.error) throw new Error(data.error.info);
-    return data;
-  },
+    getCurrent: async (query) => {
+        const { data } = await apiClient.get('/current', { params: { query } });
+        if (data.error) throw new Error(data.error.info);
+        return data;
+    },
 
-  getForecast: async (query, days = 7) => {
-    const { data } = await apiClient.get('/forecast', { params: { query, forecast_days: days } });
-    if (data.error) throw new Error(data.error.info);
-    return data;
-  },
+    getForecast: async (query, days = 7) => {
+        const { data } = await apiClient.get('/forecast', { params: { query, forecast_days: days } });
+        if (data.error) throw new Error(data.error.info);
+        return data;
+    },
 
-  getHistorical: async (query, date) => {
-    const { data } = await apiClient.get('/historical', { params: { query, historical_date: date } });
-    if (data.error) throw new Error(data.error.info);
-    return data;
-  },
+    getHistorical: async (query, date) => {
+        const { data } = await apiClient.get('/historical', { params: { query, historical_date: date } });
+        if (data.error) throw new Error(data.error.info);
+        return data;
+    },
 
-  getMarine: async (query) => {
-    const { data } = await apiClient.get('/marine', { params: { query } });
-    if (data.error) throw new Error(data.error.info);
-    return data;
-  },
+    getMarine: async (query) => {
+        const { data } = await apiClient.get('/marine', { params: { query } });
+        if (data.error) throw new Error(data.error.info);
+        return data;
+    },
 
-  autocomplete: async (query) => {
-    const { data } = await apiClient.get('/autocomplete', { params: { query } });
-    if (data.error) throw new Error(data.error.info);
-    return data;
-  },
+    autocomplete: async (query) => {
+        const { data } = await apiClient.get('/autocomplete', { params: { query } });
+        if (data.error) throw new Error(data.error.info);
+        return data;
+    },
 };
